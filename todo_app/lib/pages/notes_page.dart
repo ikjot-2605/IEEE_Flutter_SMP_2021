a9233445ee79283be9e64cc4fceeaea21246aca1
Ikjot Singh <ikjotsd@gmail.com>
2021-06-22 18:56:08 +0530

    Basic Setup of App

---
+++ todo_app/lib/pages/notes_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/model/Note.dart';
+
+class NotesPage extends StatelessWidget {
+  List<Note> notes = [];
+  NotesPage(List<Note> notes) {
+    this.notes = notes;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(notes[index].title);
+        },
+      ),
+    );
+  }
+}
